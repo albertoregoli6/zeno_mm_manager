49bf2cfc36a08cf6624a6480c1ccabd8c837328a
albertoregoli6 <albertoregoli@gmail.com>
2025-10-20 18:02:24 +0200

    deleted unnecessary variables,  new reading of json and new structure of mission for state machine

--- scripts/ammirare.py
+++ scripts/ammirare.py
@@ -4,25 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from std_msgs.msg import String
 from std_msgs.msg import Empty
-from std_msgs.msg import Float64MultiArray
 from marta_msgs.msg import NavStatus
 from zeno_mission_manager.msg import *
 
 from library import geodetic_functions as g
 from library import function as f
 
-##################################
-# INIZIALIZZAZIONE DEI PARAMETRI #
-##################################
-
-wayList    = []                 # lista dei waypoint da pubblicare a zeno_mission_manager
-WP         = []                 # lista dei waypoint ricevuti dal json
-NED_origin = np.array([0,0,0])  # origine terna NED in lat, long, depth 
-
 ####################
 # IMPORT PARAMETRI #
 ####################
 
+json_dir  = rospy.get_param("/json_dir")
 alpha_rot = rospy.get_param("/alpha_rot") # angolo fra NED e local
 
 # Area di lavoro
@@ -54,8 +46,8 @@ class planning:
 
         # Pubblicazione per stati del robot
         self.pubDeleteMission = rospy.Publisher("zeno/mm/delete_mission",Empty,queue_size=10)
-        self.pubStartMission = rospy.Publisher("zeno/mm/start_mission",Empty,queue_size=10)
-        self.pubPauseMission = rospy.Publisher("zeno/mm/pause_mission",Empty,queue_size=10)
+        self.pubStartMission  = rospy.Publisher("zeno/mm/start_mission",Empty,queue_size=10)
+        self.pubPauseMission  = rospy.Publisher("zeno/mm/pause_mission",Empty,queue_size=10)
         
         # Lettura stato della missione
         self.subZenoStatus = rospy.Subscriber("zeno/mm/mission_status", String, self.callback_ZenoStatus)
@@ -67,11 +59,14 @@ class planning:
 
         # DEFINIZIONE ATTRIBUTI DELLA CLASSE
         self.rate               = rospy.Rate(10)     # frequenza di lavoro pari a 10Hz
-        self.initWaypoint       = False              # variabile booleana usata per il calcolo in terna local dei waypoint
-        self.w_local            = []                 # lista che racchiude tutti i waypoint espressi in terna LOCAL
-        self.w_trajectory_local = []                 # lista di waypoint da pubblicare
+        self.w_local            = []                 # lista che racchiude tutti i waypoint dati espressi in terna LOCAL
+        self.w_trajectory_local = []                 # lista di waypoint locali
         self.w_trajectory       = []                 # lista di waypoint da pubblicare 
-        self.statusZeno         = ''                 # stringa che memorizza lo stato del robot
+        self.wayList            = []                 # lista dei waypoint da pubblicare a zeno_mission_manager
+
+        self.statusZeno         = ''                 # stringa che memorizza lo stato di missione del robot
+        self.mission            = ''                 # tipo di missione
+
         self.latLongVector      = []                 # posizione di Zeno (vettore 3d)
         self.firstNavStatus     = True               # primo NavStatus ottenuto
 
@@ -79,63 +74,70 @@ class planning:
         self.C_lo2ne = np.transpose(self.C_ne2lo)                                       # matrice di rotazione local2NED
         self.first_mission = True
 
-        # MISSIONE DA COMPIERE
-        global WP
-        WP, self.mission = f.read_points_from_json("/home/alberto/zeno_ws/src/zeno_mission_manager/json/niasca.json")
-
-        print("Punti letti:")
-        for pt in WP:
-            print("  %s" % (pt,))
-        print("Tipo: %s" % (self.mission,))
+        # Lettura json
+        self.json_dir      = json_dir
+        self.json          = None
+        self.time          = 0.0
+        self.bool_new_json = False
+        self.force         = True
 
     ##########################################
                     # NODE #
     ##########################################
 
     def run(self):
-        global WP
-
         while not rospy.is_shutdown():
-            # Calcolo dell origine per il sistema locale (nel waypoint piu vicino)
-            if self.initWaypoint and len(WP) != 0:
-    
-                ############## ESECUZIONE MISSIONE ##############
-                #if self.statusZeno=="IDLE":
-                if self.first_mission== True:
-                    self.first_mission = False
-                    
-                    if self.mission == "go_to_waypoint":   
-                        filtered_WP = f.filter_points_in_safety_area(WP, A, self.mission)
-                        self.w_trajectory = filtered_WP                       
-                    else:
-                        ordered_WP  = f.points_counterclockwise(WP) 
-                        filtered_WP = f.filter_points_in_safety_area(ordered_WP, A, self.mission)
 
-                        dist_lld = []
-                        for i in filtered_WP:
-                            dist_lld.append(g.lld2distance(self.latLongVector, i))
-                        min_index_lld = dist_lld.index(min(dist_lld))
-                        NED_origin = filtered_WP[min_index_lld]
+            if not self.firstNavStatus:
 
-                        # per ogni waypoint in coordinate lld vengono calcolate le coordinate in terna LOCAL
-                        for i in filtered_WP:
-                            w_local_i = f.positionLocal(i,self.C_ne2lo,NED_origin)
-                            self.w_local.append(w_local_i)
+                # Check nuovo json
+                self.WP, self.mission, self.json, self.time, self.bool_new_json = f.check_and_read_new_json(self.json_dir, self.json, self.time, self.force)
+                self.force = False
 
-                        self.w_trajectory_local = f.transetti(self.w_local)
-                        for e in self.w_trajectory_local:
-                            self.w_trajectory.append(f.waypointLocal2LL(e,self.C_lo2ne,NED_origin))
+                # Calcolo dell origine per il sistema locale (nel waypoint piu vicino)
+                if self.bool_new_json:
 
-                    
-                    wayList = f.waypointsList(self.w_trajectory)    # definizione del messaggio di waypoint
-                    
-                    #wayList = []
-                self.pubUploadMission.publish(wayList)          # viene caricata la lista di waypoint che permette al robot di passare dallo stato IDLE a READY
+                    print("-----------------")
+                    for i in range(len(self.mission)):
+                        print("Missione: ", self.mission[i], self.WP[i])
+                    print("-----------------")
+                        
+                    ############## ESECUZIONE MISSIONE ##############
+                    for i in range(len(self.mission)):
+                        if self.mission[i] == "point":   
+                            filtered_WP = f.filter_points_in_safety_area(self.WP[i], A, self.mission[i])
+                            self.w_trajectory.extend(filtered_WP) 
+                        else:
+                            ordered_WP  = f.points_counterclockwise(self.WP[i]) 
+                            filtered_WP = f.filter_points_in_safety_area(ordered_WP, A, self.mission[i])
+
+                            dist_lld = []
+                            for i in filtered_WP:
+                                dist_lld.append(g.lld2distance(self.latLongVector, i))
+                            min_index_lld = dist_lld.index(min(dist_lld))
+                            NED_origin = filtered_WP[min_index_lld]
+
+                            # per ogni waypoint in coordinate lld vengono calcolate le coordinate in terna LOCAL
+                            for i in filtered_WP:
+                                w_local_i = f.positionLocal(i,self.C_ne2lo,NED_origin)
+                                self.w_local.append(w_local_i)
+
+                            self.w_trajectory_local = f.transetti(self.w_local)
+                            for e in self.w_trajectory_local:
+                                self.w_trajectory.append(f.waypointLocal2LL(e,self.C_lo2ne,NED_origin))
+
+                    self.wayList = f.waypointsList(self.w_trajectory)    # definizione del messaggio di waypoint
+                                
+                if self.statusZeno == "IDLE" and self.wayList:
+                    self.pubUploadMission.publish(self.wayList)          # pubblicazione lista di waypoint per passare dallo stato IDLE a READY
 
                 # condizione utile ad avviare la missione su Zeno aspettando che lo stato sia READY: lo stato diventera poi RUNNING
                 if self.statusZeno == "READY":
+                    self.wayList = []
                     self.pubStartMission.publish(self.Mission)
-                    self.initWaypoint = False                                               # viene messa a False dopo aver pubblicato i waypoint
+
+                if self.statusZeno == "COMPLETED":
+                    self.pubDeleteMission.publish(self.Mission)
 
             self.rate.sleep()
 
@@ -145,9 +147,6 @@ class planning:
 
     # Callback relativa al NavsStatus
     def callback_pose(self,navStatus_msg):
-        global wayList
-        global NED_origin
-
         # posizione del robot in terna LLD (latitudine, longitudine, depth)
         latitude           = navStatus_msg.position.latitude                          # estrazione latitudine ECEF
         longitudine        = navStatus_msg.position.longitude                         # estrazione longitudine ECEF
@@ -156,7 +155,6 @@ class planning:
         self.latLongVector = np.array([latitude, longitudine, altitude])              # composizione delle 3 variabili in terna LL
 
         if self.firstNavStatus:
-            self.initWaypoint   = True
             self.firstNavStatus = False
             print("PRIMO GPS")
 
