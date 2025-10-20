8444749614683f17bc79f5269a6a84d7af2578e5
albertoregoli6 <albertoregoli@gmail.com>
2025-10-20 17:22:21 +0200

    read json in while and plot complex mission

--- scripts/plot.py
+++ scripts/plot.py
@@ -10,12 +10,15 @@ from zeno_mission_manager.msg import WaypointList
 # IMPORT PARAMETRI #
 ####################
 
+# Cartella salvataggio json
+json_dir  = rospy.get_param("/json_dir")
+
 # Area di lavoro
 A0 = np.array(rospy.get_param("/A0"))
 A1 = np.array(rospy.get_param("/A1"))
 A2 = np.array(rospy.get_param("/A2"))
 A3 = np.array(rospy.get_param("/A3"))
-A  = np.array([A0, A1, A2, A3])   # shape (4,2) o (4,3)
+A  = np.array([A0, A1, A2, A3])
 
 ####################
 #  CLASSE PLANNING #
@@ -26,7 +29,7 @@ class plot:
     def __init__(self):
         rospy.init_node('plot', anonymous=True)
 
-        # Sorgenti dati
+        # Subscriber
         self.sub_nav     = rospy.Subscriber("/nav_status", NavStatus, self.callback_pose)
         self.sub_waylist = rospy.Subscriber("/zeno/mm/upload_mission", WaypointList, self.waypoint_callback)
 
@@ -34,17 +37,21 @@ class plot:
 
         self.rate          = rospy.Rate(2)
         self.latLongVector = []              
-        self.waypoint_list = []              
+        self.waypoint_list = []     
 
-        # Missione da file JSON
-        self.WP, self.mission = f.read_points_from_json("/home/alberto/zeno_ws/src/zeno_mission_manager/json/niasca.json")
+        # Lettura json
+        self.json_dir      = json_dir
+        self.json          = None
+        self.time          = 0.0
+        self.bool_new_json = False
+        self.force         = True         
 
     def run(self):
         while not rospy.is_shutdown():
             # Converte A in lista di liste per il plot
             A_list = A[:, :2].tolist() if isinstance(A, np.ndarray) else A
 
-            # latLongVector: np.array([lat, lon]) -> [[lat, lon]]
+            # Plot zeno
             if isinstance(self.latLongVector, np.ndarray) and self.latLongVector.size >= 2:
                 zeno_point = [[float(self.latLongVector[0]), float(self.latLongVector[1])]]
             elif isinstance(self.latLongVector, (list, tuple)) and len(self.latLongVector) >= 2:
@@ -52,7 +59,15 @@ class plot:
             else:
                 zeno_point = []
 
-            # Plot
+            # Check nuovo json
+            WP, mission, self.json, self.time, self.bool_new_json = f.check_and_read_new_json(self.json_dir, self.json, self.time, self.force)
+            self.force = False
+
+            if self.bool_new_json:
+                self.WP = WP
+                self.mission = mission
+                self.bool_new_json = False
+
             f.plot_mission(A_list,self.waypoint_list,self.WP,zeno_point,self.mission)
 
             self.rate.sleep()
